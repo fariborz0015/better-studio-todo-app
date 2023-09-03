d8306ad00244708c2568324e08d0eec8dac2e856
fariborz0015 <fariborz0015@gmail.com>
2023-09-03 22:54:42 +0330

    feat: add todo container to app.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,16 @@
 import React from "react";
 import Layout from "./layout/Layout";
+import TodoConatainer from "./components/todo-container/TodoConatainer";
+ 
 
 function App() {
-  return <Layout>test</Layout>;
+  return (
+    <Layout>
+      <div className="w-full  grid grid-cols-3 gap-5 pt-12">
+        <TodoConatainer todoMode="doing" title="todo"  taskCount={3}/>
+      </div>
+    </Layout>
+  );
 }
 
 export default App;
